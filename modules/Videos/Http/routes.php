2757669c3ab7abf8f851d3afe7d904b8f2566a19
Phamnhuy <Nhuyphambkhn@gmail.com>
2015-12-01 14:27:54 +0700

    nhu y

--- modules/Videos/Http/routes.php
+++ modules/Videos/Http/routes.php
@@ -1,5 +1,5 @@
 <?php
-Route::group(['prefix' => 'manager/videos', 'namespace' => 'Modules\Posts\Http\Controllers'], function()
+Route::group(['prefix' => 'manager/videos', 'namespace' => 'Modules\Videos\Http\Controllers'], function()
 {
 	Route::get('/','VideosController@index');
 	Route::get('/add','VideosController@add');
