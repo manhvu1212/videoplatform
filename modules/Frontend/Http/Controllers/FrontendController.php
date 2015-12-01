2df5c7004275e069ccd747e90f98c3217a747d37
Phamnhuy <Nhuyphambkhn@gmail.com>
2015-12-01 17:16:08 +0700

    nhu y

--- modules/Frontend/Http/Controllers/FrontendController.php
+++ modules/Frontend/Http/Controllers/FrontendController.php
@@ -44,7 +44,7 @@ class FrontendController extends Controller {
             "company"   =>"freelancer"
         );
 
-        print_r($objVideos);die;
+        //print_r($objVideos);die;
         $objVideos['obj2']['name'] = "anh";
         $objVideos->save();
     }
