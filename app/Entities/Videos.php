bda80e58bed0d1294772a505621c887fc9df2c5c
Phamnhuy <Nhuyphambkhn@gmail.com>
2015-12-01 11:13:39 +0700

    nhu y

--- app/Entities/Videos.php
+++ app/Entities/Videos.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 class Videos extends Model
 {
     protected $fillable = [];
+    protected $connection = 'mongodb';
     protected $collection = 'Videos';
 
 }
