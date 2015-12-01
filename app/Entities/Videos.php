3d5dbc901f7aaa9acd03d6ec74418d247c3bef82
Phamnhuy <Nhuyphambkhn@gmail.com>
2015-12-01 11:12:41 +0700

    nhu y

--- app/Entities/Videos.php
+++ app/Entities/Videos.php
@@ -1,9 +1,10 @@
 <?php
-    namespace App\Entities;
-    use Illuminate\Database\Eloquent\Model;
 
-    class Videos extends Model{
-        protected  $fillable =[];
-        protected  $connection='Videos';
-    }
-?>
+namespace App\Entities;
+use Illuminate\Database\Eloquent\Model;
+class Videos extends Model
+{
+    protected $fillable = [];
+    protected $collection = 'Videos';
+
+}
