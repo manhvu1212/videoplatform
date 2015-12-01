e27d5a6fc3c3d68865cc6632772c2234fc363a74
Phamnhuy <Nhuyphambkhn@gmail.com>
2015-12-01 14:46:11 +0700

    nhu y

---
+++ modules/Videos/Resources/views/add.blade.php
@@ -0,0 +1,14 @@
+@extends('system::layouts.master')
+@section('style')
+    <link rel="stylesheet" type="text/css" href="/js/select2/select2.css" />
+    <link rel="stylesheet" type="text/css" href="/css/admin/home.css" />
+@stop
+@section('script')
+    <script src="/js/tinymce/tinymce.min.js" ></script>
+    <script src="/js/select2/select2.js"></script>
+    <script src="/js/admin/posts.js" ></script>
+    <script src="/js/jquery.tmpl.min.js" ></script>
+    <script type="text/javascript" src="/assets/global/plugins/jquery-validation/js/jquery.validate.min.js"></script>
+    <script src="/assets/admin/pages/scripts/form-validation.js"></script>
+    <script type="text/javascript" src="/assets/global/plugins/jquery-validation/js/additional-methods.min.js"></script>
+@stop
