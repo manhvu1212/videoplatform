7662146ecc36b723ae3ac66028f32ddbf56c9e78
Phamnhuy <Nhuyphambkhn@gmail.com>
2015-12-01 16:48:53 +0700

    nhu y

--- modules/Frontend/Http/Controllers/FrontendController.php
+++ modules/Frontend/Http/Controllers/FrontendController.php
@@ -25,7 +25,7 @@ class FrontendController extends Controller {
 
     public function getVideos(){
         $objVideos  = new Videos();
-        $videos = $objVideos->select('hobbit')->where(array('obj2.name' =>  'hna cogn'))->get();
+        $videos = $objVideos->select('obj2.hobbit')->where(array('obj2.name' =>  'hna cogn'))->get();
        // $videos = $objVideos->where(array('obj1'=>array("name"=>"Phamnhuy")))->get();
         echo '<pre>';
         print_r(json_decode($videos));
