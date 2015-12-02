4ca3deda40fc7884d1f8d069d1c4177b7d99b591
Phamnhuy <Nhuyphambkhn@gmail.com>
2015-12-02 11:30:31 +0700

    nhu y

--- public/js/admin/posts.js
+++ public/js/admin/posts.js
@@ -144,13 +144,7 @@ var POST = {
 
 
     upload_image: function (obj) {
-        POPUPFILE.open(function(data){
-            jQuery('#imageurl').val(data.url);
-            jQuery('#imageid').val(data._id);
-            jQuery('#div_img-dd').show();
-            jQuery('.div_img-dd').html('<img src="'+SETTINGS.domain_image+'thumbs/200/200/'+data.url+'" class="img-dd" />');
-
-        });
+       cconsole.log(obj);
     },
 
     validation_form:function(){
