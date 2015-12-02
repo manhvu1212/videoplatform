b7b9d1fa5c2842015931be4c85673f503694ed4c
Phamnhuy <Nhuyphambkhn@gmail.com>
2015-12-02 10:58:18 +0700

    nhu y

--- modules/Videos/Resources/views/add.blade.php
+++ modules/Videos/Resources/views/add.blade.php
@@ -51,17 +51,24 @@
                                     <label class="col-md-3">Titles</label>
                                     <div class="col-md-8"><input type="text" name="title" id="title" class="form-control"></div>
                                 </div>
+
                                 <div>
                                      <label class="col-md-3">Videos Url</label>
                                      <div class="col-md-8"><input type="text" name="url" id="url" class="form-control"></div>
                                 </div> 
-                                <div class="col-md-9">
-                                     <iframe width="560" height="315" src="https://www.youtube.com/embed/p4ryw5vascY" frameborder="0" allowfullscreen></iframe>
+
+                                <div>
+                                    <label class="col-md-3"></label>
+                                    <div class="col-md-8">
+                                        <iframe style="width:100%;" height="315" src="https://www.youtube.com/embed/p4ryw5vascY" frameborder="0" allowfullscreen></iframe>
+                                    </div>
                                 </div> 
+
                                 <div>
                                     <label class="col-md-3">Description</label>
                                     <div class="col-md-8"><textarea name="description" class="form-control content_post" style="height:300px;" id="description"></textarea></div>
-                                </div>                             
+                                </div> 
+
                             </div>
                             <div class="col-md-3">
                                 <p>Ypn</p>
